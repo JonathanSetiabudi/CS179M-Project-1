e48416c31e6c44f189c18807c012b087c022d43f
hikeseti101@gmail.com <hikeseti101@gmail.com>
2025-10-17 18:17:28 -0700

    converted pseudocode to real python and np implementation

--- nearest_neighbor_search.py
+++ nearest_neighbor_search.py
@@ -1,62 +1,74 @@
-# Follow General Outline Given my P1 Detailed Briefing
-# For now we will use pseudo code because we have questions for Dr. Keogh in tomorrow's lecture. 
-# import numpy as np
-# from user_interface import UI
-# import random
-# import time 
-# import math
+#Follow General Outline Given my P1 Detailed Briefing
+#For now we will use pseudo code because we have questions for Dr. Keogh in tomorrow's lecture. 
+import numpy as np
+from user_interface import UI
+import random
+import time 
+import math
 
-# def nearest_neighbor_search(data, period):
-#   time_limit = time.time + period
-#   dist_mat = create_dist_matrix(data)
-#   
-#   while time.time < time_limit:
-#       
-#
-#   return BSF_dist, BSF_order
+# landing bay is point 0
 
-# def nearest_neighbor_helper(dist_mat, random):
-#   visited = set()
-#   order = []
-#   distance = 0
-#   point = 0 # start at landing bay
-#   order.append(1)
-#   while len(visited) != len(dist_mat):
-#       if random:
-#           choose_shortest = random.randint(0,9)
-#       next_point = np.arg_min(dist_mat[point, :])
-#       while next_point in visited:
-#           dist_mat[point, next_point] = float('inf')
-#           next_point = np.arg_min(dist_mat[point, :])
-#       if choose_shortest < 1:
-#           if len(visited) < len(dist_mat)-1:
-#               dist_mat[point, np.arg_min(dist_mat[point, :])] = float('inf)
-#               while next_point in visited:
-#                   dist_mat[point, next_point] = float('inf')
-#                   next_point = np.arg_min(dist_mat[point, :])
-#       
-#       distance += dist_mat[point, next_point]
-#       order.append(next_point + 1)
-#       visited.add(next_point)
-#       point = next_point
-#   order.append(1)
-#   return distance, order
-#       
+def nearest_neighbor_search(data, period):
+    dist_mat = create_dist_matrix(data)
+    time_limit = time.time + period
+    BSF_dist, BSF_order = nearest_neighbor_helper(dist_mat.copy(), False)
+    while time.time < time_limit:
+        distance , order = nearest_neighbor_helper(dist_mat.copy(), True, BSF_dist)
+        if distance < BSF_dist:
+            BSF_dist = distance
+            BSF_order = order
+    
+    return BSF_dist, BSF_order
 
+def nearest_neighbor_helper(dist_mat, random, dist_to_beat = float('inf')):
+  visited = set()
+  order = []
+  distance = 0
+  point = 0 # start at landing bay
+  order.append(0)
+  while len(visited) != len(dist_mat):
+        if random:
+            choose_shortest = random.randint(0,9)
+        next_point = np.arg_min(dist_mat[point, :])
+        while next_point in visited:
+            dist_mat[point, next_point] = float('inf')
+            next_point = np.arg_min(dist_mat[point, :])
+        if random and choose_shortest < 1:
+            if len(visited) < len(dist_mat)-1:
+                dist_mat[point, np.arg_min(dist_mat[point, :])] = float('inf')
+                while next_point in visited:
+                    dist_mat[point, next_point] = float('inf')
+                    next_point = np.arg_min(dist_mat[point, :])
+        
+        distance += dist_mat[point, next_point]
+        if not random and distance >= dist_to_beat:
+            return float('inf'), None
+        order.append(next_point)
+        visited.add(next_point)
+        point = next_point
+  order.append(0)
+  return distance, order
+      
 
-# assumes coords given as x, y 
-# def euclid_dist(a: list, b: list):
-#   diff_y = a[1] - b[1] 
-#   diff_x = a[0] - b[0]
-#   diff_sq = diff_x**2 + diff_y**2
-#   return sqrt(diff_sq)
+# change to numpy
+# euclid = sqrt(sum (a_i - b_i)^2)
+def create_dist_matrix(data):
+    diff = 
+    np.sum(diff**2)
 
-# def create_dist_matrix(dataframe):
-#   dist_mat = [[float('inf')] * len(dataframe) for i in range len(dataframe)]
-#   for r in range(len(dataframe)):
-#       for c in range(r + 1,len(dataframe)):
-#           dist_mat[r,c] = dist_mat[c,r] = euclid_dist(dataframe[r], dataframe[c])
-#   return dist_mat
+#assumes coords given as x, y 
+def euclid_dist(a: list, b: list):
+    diff_y = a[1] - b[1] 
+    diff_x = a[0] - b[0]
+    diff_sq = diff_x**2 + diff_y**2
+    return sqrt(diff_sq)
+
+def create_dist_matrix(dataframe):
+  dist_mat = [[float('inf')] * len(dataframe) for i in range len(dataframe)]
+  for r in range(len(dataframe)):
+      for c in range(r + 1,len(dataframe)):
+          dist_mat[r,c] = dist_mat[c,r] = euclid_dist(dataframe[r], dataframe[c])
+  return dist_mat
 
 
 
--- random_search.py
+++ random_search.py
@@ -1,58 +1,52 @@
 # Follow General Outline Given my P1 Detailed Briefing
-# For now we will use pseudo code because we have questions for Dr. Keogh in tomorrow's lecture. 
-# import numpy as np
-# from user_interface import UI
-# import random
-# import time 
-# import math
+import numpy as np
+from numpy import random
+#from user_interface import UI
+import time 
+import math
 
 # check for indexing mistakes
-# def make_permutation():
-#   points = set(i+1 for i in range(n + 1))
-#   order = [1]
-#   for i in range(n-1):
-#       point = random.randint(2, n+1)
-#       if point in points:
-#           order.append(point-1)
-#           points.remove(point)
-#   order.append(0) # return to start
-#   return order
+def make_permutation(n):
+    order = [i for i in range(0,n)]
+    order.append(0)
+    order = np.array(order)
+    random.shuffle(order[1:n])
+    return order
 
-# def random_search(data, period):
-#   n = len(data)
-#   # find random permutation 
-#   BSF_dist = float('inf')
-#   BSF_order = []
-#   dist_mat = create_dist_matrix(dataframe)
-#   permutations = set()
-#   permutation = make_permutation()
-#   permutation.add(permutation)
-#   time_limit = time.time() + period
-#   while(time.time() < time_limit):
-#       distance = 0
-#       for i in range(n-1):
-#           distance += dist_matrix[i, i + 1]
-#           # if distance > BSF_dist: # add for early abandoning
-#           #    break
-#       if distance < BSF_dist:
-#           BSF_dist = distance
-#           BSF_order = permutation
-#       permutation = make_permutation()
-#       permutations.add(permuation)
-#   return math.ceil(BSF_dist), BSF_order
+def random_search(data, period):
+    n = data.shape[0]
+    # find random permutation 
+    BSF_dist = float('inf')
+    BSF_order = []
+    dist_mat = create_dist_matrix(data)
+    seen_perms = set()
+    permutation = make_permutation(n)
+    seen_perms.add(tuple(permutation))
+    time_limit = time.time() + period
+    while(time.time() < time_limit):
+        distance = 0
+        if tuple(permutation) in seen_perms:
+            continue
+        for i in range(n-1):
+            distance += dist_mat[permutation[i], permutation[i+1]]
+            # if distance > BSF_dist: # add for early abandoning
+            #    break
+        if distance < BSF_dist:
+            BSF_dist = distance
+            BSF_order = permutation
+        permutation = make_permutation()
+        seen_perms.add(tuple(permutation))
+    return math.ceil(BSF_dist), BSF_order
 
-# assumes coords given as x, y 
-# def euclid_dist(a: list, b: list):
-#   diff_y = a[1] - b[1] 
-#   diff_x = a[0] - b[0]
-#   diff_sq = diff_x**2 + diff_y**2
-#   return sqrt(diff_sq)
+def create_dist_matrix(data):
+    n = data.shape
+    dist_mat = [[float('inf')] * n for i in range(n)]
+    for r in range(n):
+        for c in range(r + 1, n):
+            dist_mat[r,c] = dist_mat[c,r] = np.linalg.norm(data[r], data[c])
+    return np.array(dist_mat)
 
-# def create_dist_matrix(dataframe):
-#   dist_mat = [[float('inf')] * len(dataframe) for i in range len(dataframe)]
-#   for r in range(len(dataframe)):
-#       for c in range(r + 1,len(dataframe)):
-#           dist_mat[r,c] = dist_mat[c,r] = euclid_dist(dataframe[r], dataframe[c])
-#   return dist_mat
-
-
+if __name__ == '__main__':
+    print(make_permutation(10))
+    data = np.array([[1,2],[5,4],[9,3]])
+    create_dist_matrix(data)
