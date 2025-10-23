3e7a31e7f3dbed43ee2b00f1390eab75164a0719
Athena Nelson <anels048@ucr.edu>
2025-10-23 09:58:03 -0700

    Add plotting over time function

--- data_visualization.py
+++ data_visualization.py
@@ -1,16 +1,13 @@
-#from nearest_neighbor_search import nearest_neighbor_search as nns
-#from random_search import random_search as rs
-from random_search import make_permutation as mp
+from nearest_neighbor_search import nearest_neighbor_search as nns
+from random_search import random_search as rs
+from user_interface import validate_file as vf
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-# plot the coordinate points and draw lines between them.
-
-# TODO: ask Dr. Keogh how do we average it over N runs continuously
 
 # create the unit square, the most basic test to see if our solution is working. 
-def create_unit_square(n, file_name):
+def create_unit_square(n, filename):
     coords = [[0,0] for i in range(n)]
     for i, coord in enumerate(coords):
         if i == 0:
@@ -21,11 +18,11 @@ def create_unit_square(n, file_name):
         coord[1-x_or_y] = random.randint(0,1)
     coords = np.array(coords)
     print(coords)
-    np.savetxt(file_name, coords, delimiter = ' ')
+    np.savetxt(filename, coords, delimiter = '   ')
     
 
-def plot_unit_square(file_name):
-    square_coords = np.loadtxt(file_name)
+def plot_unit_square(filename):
+    square_coords = np.loadtxt(filename)
     #permutation = mp(square_coords.shape)
     plt.figure(figsize=(6,6))
     sns.scatterplot(x = square_coords[:,0], y = square_coords[:, 1])
@@ -35,28 +32,67 @@ def plot_unit_square(file_name):
     plt.ylabel('Y axis')
     plt.show()
 
-
-def average_over_n_runs(n, algorithm, time, file_name):
+def average_over_n_runs(n, algorithm, time, filename):
     sum_dist = 0
-    coords = np.loadtxt(file_name)
+    coords = np.loadtxt(filename)
     for i in range(n):
         distance, _ = algorithm(coords, time)
         sum_dst += distance
     avg_dist = sum_dist / coords.shape
     return avg_dist
 
-def plot_path_taken(data, order):
+def plot_path_taken(data, order, filename):
     reordered_data = data[order]
     plt.figure(figsize=(6,6))
     plt.plot(reordered_data[:, 0], reordered_data[:,1], marker = 'o', markerfacecolor = 'blue', linestyle = '-', color = "green")
     plt.title('Order taken')
     plt.xlabel('X-axis')
     plt.ylabel('Y-axis')
+    plt.savefig(filename)
+
+def plot_over_time(distances, algorithm, time, trials):
+    plt.figure(figsize=(6,6))
+    print(distances.shape)
+    plt.plot(distances, np.arange(0, distances))
+    plt.title(f'{algorithm} average best so far distance over {time} seconds, {trials} trials')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Distance')
     plt.show()
 
+def get_average_distances(distances):
+    return np.sum(distances, axis=0)
+
+def save_distances(distances, filename):
+    np.savetxt(filename, distances)
+
 if __name__ == '__main__':
-    # create_unit_square(100,"unitSquare.txt")
-    plot_unit_square("unitSquare.txt")
-    unit_square = np.loadtxt("unitSquare.txt")
-    order = mp(unit_square.shape[0])
-    plot_path_taken(unit_square, order)
+    filename = "data/256Cashew.txt"
+    #create_unit_square(100,"data/100unitSquare.txt")
+    #plot_unit_square(filename)
+    locations = vf(filename)
+
+    # algorithm = 'RS'
+    # time = 10
+    # trials = 3
+    # distances = []
+    # for i in range(trials):
+    #     dist, order, over_time = rs(locations, 10, True)
+    #     distances.append(over_time)
+
+    # for d in distances:
+    #     print(len(d))
+
+    # distances = np.vstack(distances)
+    # print(distances.shape)
+    # print(distances)
+    # print(get_average_distances(distances))
+    # save_distances(distances, f"res/{algorithm}{trials}T{time}S")
+    # plot_over_time(get_average_distances(distances), "Random Search", time, trials)
+    #plot_path_taken(locations, order)
+
+    time = [1, 100, 1000]
+    algorithm = "RS"
+    for t in time:
+        dist, order = rs(locations, t)
+        plot_path_taken(locations, order, f"res/path_visuals/{algorithm}{dist}256Cashew{t}S.png")
+        
