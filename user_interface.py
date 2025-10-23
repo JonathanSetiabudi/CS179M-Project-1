f1b0b6f1cd98653e28a8d1e70e47ca606b77745c
Athena Nelson <anels048@ucr.edu>
2025-10-23 09:19:50 -0700

    Add file validation

--- user_interface.py
+++ user_interface.py
@@ -1,16 +1,18 @@
-#import numpy
-#import pandas as pd
-#import time
+import numpy as np
+import pandas as pd
+import time
+import os
 
-# def validate_file(string filename):
-#   if filename.endswith(".txt"):
-#       raise error("This file is not a .txt file. ABORT")
-#   if not os.path.exists()
-#       raise error("This file does not exist. ABORT")
-#   df = pd.read_csv(fileName)
-#   if len(df) > 256:
-#       raise error("This file has more than 256 points. ABORT")
-#   
+# TODO: throw exception or ask for input again?
+def validate_file(filename):
+    if not filename.endswith(".txt"):
+        raise Exception("This file is not a .txt file. ABORT")
+    if not os.path.exists(filename):
+        raise Exception("This file does not exist. ABORT")
+    df = pd.read_csv(filename, sep="   ", engine='python', header=None)
+    if len(df) > 256:
+        raise Exception("This file has more than 256 points. ABORT")
+    return df.to_numpy()
 
 # def main():
 #   print("ComputeDronePath")
@@ -29,8 +31,13 @@
 #   print("Route written to disk as ")
 
 
-#user never hits enter -->
-#i will keep track of when the user hits enter
-#the time when the user presses enter is saved
-#i call the random_search func on the time that the user presses enter 
-#i still output the distance for the times 5, 10, 15 on the terminal 
+# user never hits enter -->
+# i will keep track of when the user hits enter
+# the time when the user presses enter is saved
+# i call the random_search func on the time that the user presses enter 
+# i still output the distance for the times 5, 10, 15 on the terminal 
+
+if __name__ == '__main__':
+    filename = 'data/32Almonds.txt'
+    df = validate_file(filename)
+    print(df)
