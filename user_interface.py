17ccabdc57804710238d8257f8d4413d428dfd9f
Athena Nelson <anels048@ucr.edu>
2025-10-25 20:03:32 -0700

    Implement desired ui

--- user_interface.py
+++ user_interface.py
@@ -1,9 +1,17 @@
+from nearest_neighbor_search import nearest_neighbor_search as nns
+from random_search import random_search as rs
 import numpy as np
 import pandas as pd
 import time
 import os
 
-# TODO: throw exception or ask for input again?
+def search(data, period, algo):
+    if algo == 'RS':
+        dist, order = rs(data, period)
+    if algo == 'NNS':
+        dist, order = nns(data, period)
+    return dist, order
+
 def validate_file(filename):
     if not filename.endswith(".txt"):
         raise Exception("This file is not a .txt file. ABORT")
@@ -14,21 +22,18 @@ def validate_file(filename):
         raise Exception("This file has more than 256 points. ABORT")
     return df.to_numpy()
 
-# def main():
-#   print("ComputeDronePath")
-#   fileName = input("Enter the name of the file:")
-#   validate_file(fileName)
-#   df = pd.read_csv(fileName)
-#   nodes = len(df)
-#   print("There are 136 nodes, computing route...")
-#   print("Shortest Route Discovered So Far")
-#   dist_5, ord_5 = random_search(, 5)
-#   print(dist_5)
-#   dist_10, ord_10 = random_search(, 10)
-#   print(dist_5)
-#   dist_15, ord_15 = random_search(, 15)
-#   print(dist_5)
-#   print("Route written to disk as ")
+def main():
+    print("ComputeDronePath")
+    input_file = input("Enter the name of the file: ")
+    path = "data"
+    data = validate_file(f"{path}/{input_file}")
+    n = data.shape[0]
+    print(f"There are {n} nodes, computing route...")
+    print("Shortest Route Discovered So Far")
+    dist, order = search(data, period=12.5, algo='NNS')
+    output_file = f"{input_file}_SOLUTION_{dist}.txt"
+    print(f"Route written to disk as {output_file}")
+    np.savetxt(f"res/{output_file}", order)
 
 
 # user never hits enter -->
@@ -38,6 +43,4 @@ def validate_file(filename):
 # i still output the distance for the times 5, 10, 15 on the terminal 
 
 if __name__ == '__main__':
-    filename = 'data/32Almonds.txt'
-    df = validate_file(filename)
-    print(df)
+    main()
